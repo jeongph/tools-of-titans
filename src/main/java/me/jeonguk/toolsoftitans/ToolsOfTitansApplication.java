1e7b6024be57075f8b5d99c60fdfaf37f8e03022
JeongUk Park <dnr1105@gmail.com>
2024-10-26 23:15:53 +0900

    init: 프로젝트 초기화 with start.spring.io

---
+++ src/main/java/me/jeonguk/toolsoftitans/ToolsOfTitansApplication.java
@@ -0,0 +1,13 @@
+package me.jeonguk.toolsoftitans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsOfTitansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsOfTitansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/jeonguk/toolsoftitans/ToolsOfTitansApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jeonguk.toolsoftitans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsOfTitansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
